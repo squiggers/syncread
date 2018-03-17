2f6a1c007ee50e1d1bab730cded210682a226e22
Sam Ehret <ehret.sam@yahoo.com>
2018-03-17 14:45:20 -0500

    edit static file location

--- mybooks/settings.py
+++ mybooks/settings.py
@@ -14,7 +14,7 @@ import os
 from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -151,4 +151,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
