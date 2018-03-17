05b4ab2b25a73e24473eecfb6b4a4d6d66427611
Sam Ehret <ehret.sam@yahoo.com>
2018-03-17 14:14:31 -0500

    change static root dir

--- mybooks/settings.py
+++ mybooks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z97@l)@85$9lf6x9_kl!dbh8=c==jhman9-igsu6!_+8dd6)p9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://syncread.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://syncread.herokuapp.com/']
 
 
 # Application definition
@@ -149,7 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
